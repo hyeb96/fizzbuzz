3456473ac5e446edc6fcd677356b766ba064da72
hyeb96 <eban.hdit@gmail.com>
2022-08-22 10:13:30 -0400

    Initial commit

---
+++ Fizzbuzz.java
@@ -0,0 +1,16 @@
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz");
+            } else {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+        }
+    }
+}
